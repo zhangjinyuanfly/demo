cf9f9a9ade7737095b582d1a289155ed561bec0c
zhangjinyuan <zhangjinyuan@huajiao.tv>
2019-03-29 18:15:33 +0800

    update  H5 SHARE

--- demo/README.md
+++ demo/README.md
@@ -14,7 +14,30 @@
 + loadDataWithBaseURL 使用cache部分，8.0以上有坑，会导致goBack失效。
 
 ## 3.状态
-+ onResume
++ onResume 单独webview使用
 + onPause
-+ resumeTimers
++ resumeTimers 全局webview使用
 + pauseTimers
+
+## 4.前进后退
++ canGoBack  goBack
++ canGoForward goForward
++ goBackOrForward(step) step 负数：goBack，正数：goForward。 数字表示步数
+
+## 5.WebSettings
++ setJavaScriptEnabled
++ setUseWideViewPort 将图片调整到适合webview的大小 
++ setLoadWithOverviewMode 缩放至屏幕的大小
++ setLoadsImagesAutomatically 自动加载图片
+
+## 4.缓存
++ setCacheMode 设置webview cacheMode 4种mode
++ chrome cache 自动创建 /data/data/包名/cache/org.chromium.android_webview/
++ setJavaScriptEnabled
++ 
+
+
+
+WebView(Context context, AttributeSet attrs, int defStyleAttr) defStyleAttr不能传入0，否则会导致软键盘无法弹出
+
+webview.addJavascriptInterface 在8.0手机上。只是第一个add进入的会生效。
--- demo/app/src/main/java/com/zjy/demo/h5/H5Activity.kt
+++ demo/app/src/main/java/com/zjy/demo/h5/H5Activity.kt
@@ -1,24 +1,24 @@
 package com.zjy.demo.h5
 
 import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Environment
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.webkit.*
 import android.widget.Button
+import android.widget.TextView
 import com.zjy.demo.R
 
 
 class H5Activity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(v: View?) {
-        when(v?.id) {
-            R.id.callalert -> clickAlert()
-        }
 
+    private fun startH5() {
+        var intent = Intent(this@H5Activity, H5Activity::class.java)
+        startActivity(intent)
     }
 
     private fun clickAlert() {
@@ -45,7 +45,18 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
     }
 
     var mWebView : WebView ?= null
-    var mBtnCallAlert : Button ?= null
+    var mBtnCallAlert : TextView ?= null
+    var mBtnStartNewH5: TextView ?= null
+    var mBtnResume: TextView ?= null
+    var mBtnPause: TextView ?= null
+    var mBtnResumeTimer: TextView ?= null
+    var mBtnPauseTimer: TextView ?= null
+    var mBtnGoBack: TextView ?= null
+    var mBtnGoGorward: TextView?= null
+    var mBtnClearCache: TextView?= null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.e("zjy","H5Activity onCreate")
         super.onCreate(savedInstanceState)
@@ -54,6 +65,24 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
 
         mBtnCallAlert = findViewById(R.id.callalert)
         mBtnCallAlert?.setOnClickListener(this)
+        mBtnStartNewH5 = findViewById(R.id.start_h5)
+        mBtnStartNewH5?.setOnClickListener(this)
+        mBtnResume = findViewById(R.id.resume)
+        mBtnResume?.setOnClickListener(this)
+        mBtnPause = findViewById(R.id.pause)
+        mBtnPause?.setOnClickListener(this)
+        mBtnResumeTimer = findViewById(R.id.resumetimer)
+        mBtnResumeTimer?.setOnClickListener(this)
+        mBtnPauseTimer = findViewById(R.id.pausetimer)
+        mBtnPauseTimer?.setOnClickListener(this)
+        mBtnGoBack = findViewById(R.id.goback)
+        mBtnGoBack?.setOnClickListener(this)
+        mBtnGoGorward = findViewById(R.id.goforward)
+        mBtnGoGorward?.setOnClickListener(this)
+        mBtnClearCache = findViewById(R.id.clearcache)
+        mBtnClearCache?.setOnClickListener(this)
+
+
 
         mWebView = findViewById(R.id.webview)
         mWebView?.settings?.cacheMode = WebSettings.LOAD_DEFAULT
@@ -66,20 +95,22 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
         //设置自适应屏幕，两者合用
         mWebView?.settings?.useWideViewPort = false//将图片调整到适合webview的大小
         mWebView?.settings?.loadWithOverviewMode = false// 缩放至屏幕的大小
-        mWebView?.settings?.databaseEnabled = true// 不再维护 database/webview.db 没有了
-//        mWebView?.settings?.databasePath = cacheDir.path// 5.1
-        mWebView?.settings?.domStorageEnabled = true
 
+//        mWebView?.settings?.databaseEnabled = true// 不再维护 database/webview.db 没有了
+        mWebView?.settings?.domStorageEnabled = true// 打开domcache
 
         mWebView?.settings?.setAppCacheEnabled(true)
         mWebView?.settings?.setAppCachePath(cacheDir.path)
         mWebView?.settings?.setAppCacheMaxSize(5*1024*1024)
 
 
-        mWebView?.loadUrl("file:///android_asset/h5/javascript.html")
+
+//        mWebView?.loadUrl("file:///android_asset/h5/javascript.html")
 //        mWebView?.loadUrl("file:///android_asset/h5/timecountdown.html")
 //        mWebView?.loadUrl("http://www.huajiao.com")
-
+//        mWebView?.loadUrl("http://www.qq.com")
+        mWebView?.loadUrl("https://activity.huajiao.com/web/share/banner/2018/pgcBuy/index.html?userId=")
+//        mWebView?.loadUrl("https://test.huajiao.com/h5_plugin/test.html")
         mWebView?.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 Log.e("zjy","shouldOverrideUrlLoading url = "+url)
@@ -94,6 +125,7 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
             override fun onPageFinished(view: WebView?, url: String?) {
                 Log.e("zjy","onPageFinished url = "+url)
                 super.onPageFinished(view, url)
+                writeData()
             }
 
             override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
@@ -103,6 +135,11 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
 
             override fun shouldInterceptRequest(view: WebView?, request: WebResourceRequest?): WebResourceResponse? {
                 return super.shouldInterceptRequest(view, request)
+            }
+
+            override fun onLoadResource(view: WebView?, url: String?) {
+                super.onLoadResource(view, url)
+                Log.e("zjy","onLoadResource = "+url)
             }
         }
 
@@ -129,14 +166,46 @@ class H5Activity : AppCompatActivity(), View.OnClickListener {
 
     }
 
+    fun writeData() {
+        val key = "token"
+        val val1 = "1234455678"
+        val key2 = "uid"
+        val val2 = "10000001"
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            mWebView?.evaluateJavascript("window.localStorage.setItem('$key','$val1');", null)
+            mWebView?.evaluateJavascript("window.localStorage.setItem('$key2','$val2');", null)
+        } else {
+            mWebView?.loadUrl("javascript:localStorage.setItem('$key','$val1');")
+            mWebView?.loadUrl("javascript:localStorage.setItem('$key2','$val2');")
+        }
+    }
+
     override fun onResume() {
         super.onResume()
-        mWebView?.resumeTimers()
+//        mWebView?.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        mWebView?.pauseTimers()
+//        mWebView?.onPause()
+    }
+
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.callalert -> clickAlert()
+            R.id.start_h5 -> startH5()
+            R.id.resumetimer -> mWebView?.resumeTimers()
+            R.id.pausetimer -> mWebView?.pauseTimers()
+
+            R.id.resume -> mWebView?.onResume()
+            R.id.pause -> mWebView?.onPause()
+
+            R.id.goback -> mWebView?.goBackOrForward(-1)
+            R.id.goforward -> mWebView?.goBackOrForward(2)
+            R.id.clearcache -> mWebView?.clearCache(true)
+        }
+
     }
 
     override fun onDestroy() {
