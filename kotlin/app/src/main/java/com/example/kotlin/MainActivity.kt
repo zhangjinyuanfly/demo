6cdbca32f6c27a832f0b6d54f9a82debf0642831
unknown <zhangjinyuan@service.360ityun.com>
2019-10-29 21:47:57 +0800

    add kotlin

---
+++ kotlin/app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    suspend fun getNet(){
+    }
+
+
+
+}
---
+++ kotlin/lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,9 @@
+package com.example.lib;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+    }
+
+}
